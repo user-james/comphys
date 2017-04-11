576099e0096a5e4e157727366482e3c743085681
James Home <113477198@umail.ucc.ie>
2017-04-11 22:26:13 +0100

    some commenting done

--- liquid1/ex1/data/bulk_mod.py
+++ liquid1/ex1/data/bulk_mod.py
@@ -1,41 +1,49 @@
+# CALCULATES BULK MODULUS
+# SEE SCRIPT 'THERMAL_EXP' AS IT WORKS IN A VERY SIMILAR WAY
+
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 if __name__=='__main__':
 
+
+    # sets up arrays for densities, lengths and temperatures used
     lengths = np.arange(4.47, 4.15,-0.05)
     densities = 80*lengths**(-3)
     temperatures = np.genfromtxt("length_4.17/energy_vs_temp")[:, 0]
-    thermal_exp = []
+    data = []
     
     for i in range(len(temperatures)):
         energies = []
+
+        # grabs energies for different densities given the temperature
         for l in lengths:
             energies.append(np.genfromtxt("length_{:.2f}/energy_vs_temp".format(l))[i])
         energies = np.array(energies)
         energies = energies[:, 1]
         min_e = energies[0]
         ind = 0
+
+        # calculates the minimum energy for this temperature
+        # records minimum energy and index where it occurs
         for j in range(len(energies)):
             if(energies[j] < min_e):
                 min_e = energies[j]
                 ind = j
 
-        thermal_exp.append([densities[ind], min_e])
+        # stores minimum energy and the density where it occurs for the given temperature
+        data.append([densities[ind], min_e])
 
     
-    thermal_exp = np.array(thermal_exp)
-    thermal_exp[:, 0] = 80/thermal_exp[:, 0]
+    data = np.array(data)
+    # converts densities to volumes
+    data[:, 0] = 80/data[:, 0]
     
-    bulk_mods = thermal_exp[:, 1] / thermal_exp[:, 0]
+    # calculates bulk modulus
+    bulk_mods = data[:, 1] / data[:, 0]
     avg_bulk_mod = abs(sum(bulk_mods) / len(bulk_mods))
-    print(avg_bulk_mod)
-    print(thermal_exp)
+    print("Bulk Modulus =", avg_bulk_mod)
     
-    #print("Bulk Modulus =", expansivity)
-    #plt.xlabel("Temperature")
-    #plt.ylabel("Volume")
-    #plt.plot(thermal_exp[:, 0], thermal_exp[:, 1])
-    #plt.savefig("../plots/vol_vs_temp.jpg")
-    #plt.show()
+    
--- liquid1/ex1/data/thermal_exp.py
+++ liquid1/ex1/data/thermal_exp.py
@@ -1,9 +1,13 @@
+# CALCULATES THE THERMAL EXPANSIVITY AND PLOTS VOLUME VS ENERGY
+
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 if __name__=='__main__':
 
+    # creating arrays for lengths, densities and temperatures used
     lengths = np.arange(4.47, 4.15,-0.05)
     densities = 80*lengths**(-3)
     temperatures = np.genfromtxt("length_4.17/energy_vs_temp")[:, 0]
@@ -11,28 +15,38 @@ if __name__=='__main__':
     
     for i in range(len(temperatures)):
         energies = []
+
+        # grabs energies for each denisty given the temperature
         for l in lengths:
             energies.append(np.genfromtxt("length_{:.2f}/energy_vs_temp".format(l))[i])
         energies = np.array(energies)
         energies = energies[:, 1]
         min_e = energies[0]
         ind = 0
+
+        # records minimum energy and index at which it occurs
+        # this is the minimum energy across different densities for a single temperature
         for j in range(len(energies)):
             if(energies[j] < min_e):
                 min_e = energies[j]
                 ind = j
-
+        
+        # stores density and temperature for point where density vs energy graph is minimum
         thermal_exp.append([temperatures[ind], densities[ind]])
 
     
     thermal_exp = np.array(thermal_exp)
     
+    # calculates the slope of the density vs energy plot and thus finds the thermal expansivity
     slope = -(thermal_exp[-1, 1] - thermal_exp[0, 1]) / (thermal_exp[-1, 0] - thermal_exp[0, 0])
     expansivity = 2*slope / (thermal_exp[-1, 1] + thermal_exp[0, 1])
+    print("Thermal Expansitivty =", expansivity)
 
+    # converts stored densities to volumes
     thermal_exp[:, 1] = 80/thermal_exp[:, 1]
 
-    print("Thermal Expansitivty =", expansivity)
+
+    # formats and plots volume vs temperature
     plt.xlabel("Temperature")
     plt.ylabel("Volume")
     plt.plot(thermal_exp[:, 0], thermal_exp[:, 1])
